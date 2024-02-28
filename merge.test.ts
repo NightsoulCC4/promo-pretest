c51e8c42da6884068f338c7c98d301ac2e3daff6
Nattatat <StickyCoffee8360@gmail.com>
2024-02-28 10:01:58 +0700

    first commit

---
+++ merge.test.ts
@@ -0,0 +1,10 @@
+//import.
+import { merge } from './merge';
+
+describe('merge function', () => {
+    it('should merge sorted arrays', () => {
+        // test case.
+        const result = merge([1, 3, 5], [4, 2, 0], []);
+        expect(result).toEqual([0, 1, 2, 3, 4, 5]);
+    });
+});
---
+++ merge.ts
@@ -0,0 +1,10 @@
+
+
+export function merge(collection1: number[], collection2: number[], collection3: number[]): number[] {
+
+    let iResult: number[];
+
+    console.log(collection1);
+
+    return [0,1];
+}
